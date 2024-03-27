d5ad4e6908a923c4cc42206de33cd814119cde5d
Mateusz Oślizło <amti4osl@gmail.com>
2024-03-27 13:00:32 +0100

    make build

---
+++ index.51100d2c.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var c=document.createElement("li");c.textContent=a,Object.keys(t[a]).length>0&&e(c,t[a]),r.append(c)}n?n.append(r):(r.id="tree",document.body.prepend(r))}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.51100d2c.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,35 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const root = document.createElement("ul");
+    for(const info in data){
+        const branch = document.createElement("li");
+        branch.textContent = info;
+        if (Object.keys(data[info]).length > 0) createTree(branch, data[info]);
+        root.append(branch);
+    }
+    if (element) element.append(root);
+    else {
+        root.id = "tree";
+        document.body.prepend(root);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
